6474a1a20e96ed891b004a931097733143b532ef
Kevin Harrison <kharrison7@elon.edu>
2017-09-13 10:46:41 -0400

    This adds some bill murray info.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,42 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+   <br />
+
+
+   <nav className="navbar">
+           <h1 className="title">Favorite Murray</h1>
+         </nav>
+         <main className="main-body">
+           <div className="top">
+             <div className="image-one">
+               <img src="https://www.fillmurray.com/200/300" alt="Fill Murray Pictures" />
+             </div>
+             <p className="top-paragraph">
+               Lorem ipsum dolor sit amet, consectetur adipisicing.
+             </p>
+           </div>
+           <div className="bottom">
+             <div className="image-two">
+               <img src="https://www.fillmurray.com/300/300" alt="Fill Murray Pictures" />
+             </div>
+             <p className="bottom-paragraph">
+               Lorem ipsum dolor sit amet, consectetur adipisicing.
+             </p>
+           </div>
+         </main>
+         <div className="murrayinator">
+           <h3>What is your favorite Murray Movie?</h3>
+           <form className="murray-form" action="" method="">
+             <label htmlFor="murray-movie">Type your favorite movie here...</label>
+             <input type="text" id="murray-movie" />
+             <button>Submit</button>
+           </form>
+         </div>
+         <div className="answer">
+           <h4 className="murray-display"></h4>
+         </div>
+  </div>
     );
   }
 }
